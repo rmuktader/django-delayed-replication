25114f12156a7b0fdac441ad81612cc018f8299d
Rayhan Muktader <rmuktader@gmail.com>
2021-11-02 18:52:29 -0400

    Added script to check if the slave DB container is running before running Django container. You need to stop and restart the stack if the slave container is not running.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Waiting for postgres..."
+echo "Waiting for postgres master..."
 
 while ! getent hosts postgresql-master ; do
   sleep 1
@@ -10,6 +10,20 @@ while ! nc -z "${DB_HOST}" "${DB_PORT}"; do
   sleep 1
 done
 
-echo "PostgreSQL started"
+echo "PostgreSQL master started"
+
+
+echo "Waiting for postgres slave..."
+
+while ! getent hosts postgresql-slave ; do
+  sleep 1
+done
+
+while ! nc -z "${DB_REPLICA_NAME}" "${DB_PORT}"; do
+  sleep 1
+done
+
+echo "PostgreSQL slave started"
+
 
 exec "$@"
