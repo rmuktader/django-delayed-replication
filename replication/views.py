54b1e3314dde43d750140b4102005d56c6f3e97f
Rayhan Muktader <rmuktader@gmail.com>
2021-11-03 00:30:31 -0400

    We update a record in the write database and instantly read that record from the read database. Most of the time we see that the timestamps don't match. The read database is lagging behind the write database. Meaning, replication hasn't taken place yet.

--- replication/views.py
+++ replication/views.py
@@ -1,4 +1,19 @@
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import ReplicationTestRecord
+from django.db import connections
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the replication index.")
+    now = timezone.now()
+    rec, _ = ReplicationTestRecord.objects.get_or_create(id=1, defaults={'change_date': now,'test_text': now})
+    rec.test_text = now
+    rec.change_date = now
+    rec.save()
+
+    try:
+        read = ReplicationTestRecord.objects.get(id=1)
+        read_ts = read.test_text
+    except ReplicationTestRecord.DoesNotExist:
+        read_ts = 'Unknown'
+
+    return HttpResponse(f"Write TS: {now}, Read TS: {read_ts}")
